f381124021e49d0f03bdd37519423c69cb4e2e16
Guy Halse <guy@tenet.ac.za>
2016-06-28 14:47:58 +0200

    tabs -> spaces

--- lib/Auth/Process/AttributeFromEntity.php
+++ lib/Auth/Process/AttributeFromEntity.php
@@ -10,15 +10,15 @@ class sspmod_entattribs_Auth_Process_AttributeFromEntity extends SimpleSAML_Auth
 {
     /** @var bool|false Should we replace existing attributes? */
     private $replace = false;
-	
-	/** @var array Attributes we have already replaced */
-	private $replaced = array();
-	
-	/** @var array Should we skip looking in this metadata */
-	private $skip = array();
-	
-	/** @var array Map from Entity Attribute name to attribute name */
-	private $map = array();
+    
+    /** @var array Attributes we have already replaced */
+    private $replaced = array();
+    
+    /** @var array Should we skip looking in this metadata */
+    private $skip = array();
+    
+    /** @var array Map from Entity Attribute name to attribute name */
+    private $map = array();
 
     /**
      * Initialize this filter, parse configuration.
@@ -31,29 +31,29 @@ class sspmod_entattribs_Auth_Process_AttributeFromEntity extends SimpleSAML_Auth
         parent::__construct($config, $reserved);
         assert('is_array($config)');
 
-		foreach($config as $origName => $newName) {
-			if (is_int($origName)) {
-				if($newName === '%replace') {
-					$this->replace = true;
-				}
-				if ($newName === '%skipsource' or $newName === '%sourceskip') {
-					array_push($this->skip, 'Source');
-				}
-				if ($newName === '%skipdest' or $newName === '%destskip') {
-					array_push($this->skip, 'Destination');
-				}
-				/* might want to make this handle loadable maps, a`la core:AttributeMap */
-			
-			} elseif (is_string($origName)) {
-				$this->map[$origName] = $newName;
-			} else {
-				throw new SimpleSAML_Error_Exception('AttributeFromEntity: invalid config object, cannot create map');
-			}
-		}
-		
-		if (count($this->map) === 0) {
-			throw new SimpleSAML_Error_Exception('AttributeFromEntity: attribute map is empty. Config error?');
-		} 
+        foreach($config as $origName => $newName) {
+            if (is_int($origName)) {
+                if($newName === '%replace') {
+                    $this->replace = true;
+                }
+                if ($newName === '%skipsource' or $newName === '%sourceskip') {
+                    array_push($this->skip, 'Source');
+                }
+                if ($newName === '%skipdest' or $newName === '%destskip') {
+                    array_push($this->skip, 'Destination');
+                }
+                /* might want to make this handle loadable maps, a`la core:AttributeMap */
+            
+            } elseif (is_string($origName)) {
+                $this->map[$origName] = $newName;
+            } else {
+                throw new SimpleSAML_Error_Exception('AttributeFromEntity: invalid config object, cannot create map');
+            }
+        }
+        
+        if (count($this->map) === 0) {
+            throw new SimpleSAML_Error_Exception('AttributeFromEntity: attribute map is empty. Config error?');
+        } 
     }
 
     /**
@@ -65,41 +65,41 @@ class sspmod_entattribs_Auth_Process_AttributeFromEntity extends SimpleSAML_Auth
     {
         assert('is_array($request)');
         assert('array_key_exists("Attributes", $request)');
-		assert('array_key_exists("entityid", $request["Source"])');
-		assert('array_key_exists("entityid", $request["Destination"])');
+        assert('array_key_exists("entityid", $request["Source"])');
+        assert('array_key_exists("entityid", $request["Destination"])');
 
         $attributes =& $request['Attributes'];
-				
-		foreach (array('Source', 'Destination') as $source) {
-			if (in_array($source, $this->skip)) { continue; }
-			if (!array_key_exists('EntityAttributes', $request[$source])) { continue; }
-			
-			foreach ($request[$source]['EntityAttributes'] as $entityAttributeName => $entityAttributeValue) {
-				SimpleSAML_Logger::debug('AttributeFromEntity: found entity attribute ' . 
-					$entityAttributeName . ' in ' . $source . ' metadata -> ' . 
-					var_export($entityAttributeValue, true)
-				);
+                
+        foreach (array('Source', 'Destination') as $source) {
+            if (in_array($source, $this->skip)) { continue; }
+            if (!array_key_exists('EntityAttributes', $request[$source])) { continue; }
+            
+            foreach ($request[$source]['EntityAttributes'] as $entityAttributeName => $entityAttributeValue) {
+                SimpleSAML_Logger::debug('AttributeFromEntity: found entity attribute ' . 
+                    $entityAttributeName . ' in ' . $source . ' metadata -> ' . 
+                    var_export($entityAttributeValue, true)
+                );
 
-				if (array_key_exists($entityAttributeName, $this->map)) {
-					SimpleSAML_Logger::info('AttributeFromEntity: found entity attribute mapping ' .
-						$entityAttributeName . ' -> ' . $this->map[$entityAttributeName]);
+                if (array_key_exists($entityAttributeName, $this->map)) {
+                    SimpleSAML_Logger::info('AttributeFromEntity: found entity attribute mapping ' .
+                        $entityAttributeName . ' -> ' . $this->map[$entityAttributeName]);
 
-					/* 
-					 * because we pass through this twice, we need to keep 
-					 * track of replacements we've made vs replacements of
-					 * the original SAML attributes. 
-					 */
-					if ($this->replace === true and !in_array($this->map[$entityAttributeName],$this->replaced)) {
-						$attributes[$this->map[$entityAttributeName]] = array($entityAttributeValue);
-						$this->replaced[$this->map[$entityAttributeName]] = true;
-					} elseif (array_key_exists($this->map[$entityAttributeName], $attributes)) {
+                    /* 
+                     * because we pass through this twice, we need to keep 
+                     * track of replacements we've made vs replacements of
+                     * the original SAML attributes. 
+                     */
+                    if ($this->replace === true and !in_array($this->map[$entityAttributeName],$this->replaced)) {
+                        $attributes[$this->map[$entityAttributeName]] = array($entityAttributeValue);
+                        $this->replaced[$this->map[$entityAttributeName]] = true;
+                    } elseif (array_key_exists($this->map[$entityAttributeName], $attributes)) {
                         array_push($attributes[$this->map[$entityAttributeName]],
                             $entityAttributeValue);
                     } else {
                         $attributes[$this->map[$entityAttributeName]] = array($entityAttributeValue);
                     }
-				}
-			}
-		}
+                }
+            }
+        }
     }
 }
